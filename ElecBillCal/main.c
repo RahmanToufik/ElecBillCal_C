d140d9d80e59d63af7430b84e08611e595308d49
Toufikur Rahman <toufiikur@gmail.com>
2023-08-10 23:02:30 +0600

    Initial Commit

---
+++ ElecBillCal/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ElecBillCal
+//
+//  Created by Toufikur Rahman on 8/10/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
